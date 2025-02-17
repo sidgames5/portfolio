6cba9e0f30440b42e86e32929fa67b7f09a0807f
SidGames5 <saytleader@outlook.com>
2025-02-17 17:32:45 -0500

    afasgasgdgg

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,4 @@ set -e
 
 npm run build
 
-if git branch --list | grep -q 'static'; then
-    git branch -D static
-fi
-
-if ! git subtree split --prefix build -b static; then
-    echo "Subtree split failed."
-    exit 1
-fi
-
-if ! git push origin static:static --force; then
-    echo "Failed to push."
-    git branch -D static
-    exit 1
-fi
-
-git branch -D static
+echo "do the rest manually im too lazy to make a script"
