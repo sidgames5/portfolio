76aaaa90e16c34d7aff69dd605613e67a51d0655
SidGames5 <saytleader@outlook.com>
2025-02-17 17:25:57 -0500

    fixed again

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,21 @@
-#!/usr/bin/env sh
+#!/usr/bin bash
 set -e
 
 npm run build
-git checkout -b static
-git add build
-git commit -m "new deployment"
-git push origin -u static
-git checkout -b master
+
+if git branch --list | grep -q 'static'; then
+    git branch -D static
+fi
+
+if ! git subtree split --prefix static -b static; then
+    echo "Subtree split failed."
+    exit 1
+fi
+
+if ! git push origin static:static --force; then
+    echo "Failed to push."
+    git branch -D static
+    exit 1
+fi
+
+git branch -D static
