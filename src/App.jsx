26e6e60b0b4c0ca2e70efdbea02e67fc43ca91ee
SidGames5 <saytleader@outlook.com>
2025-02-24 08:35:09 -0500

    added mobile optimizations

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,20 @@ import Sidebar from "./Sidebar";
 function App() {
     return (
         <main className="flex flex-col justify-center items-center">
-            <div className="flex flex-row justify-center align-middle gap-16 p-8">
+            <div className="flex sm:flex-row flex-col justify-center align-middle gap-16 p-8">
                 <Sidebar />
                 <div className="flex flex-col gap-2 h-full">
                     <h2 className="text-2xl">Projects</h2>
                     {/* <div className="flex flex-wrap gap-4 h-full"> */}
-                    <div className="h-full grid grid-cols-3 gap-4">
+                    <div className="h-full grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-4">
                         {PersonalData.projects.map(ProjectCard)}
                     </div>
                     <h2 className="text-2xl">Education</h2>
-                    <div className="h-full grid grid-cols-3 gap-4">
+                    <div className="h-full grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-4">
                         {PersonalData.education.map(EducationCard)}
                     </div>
                     <h2 className="text-2xl">Competitions</h2>
-                    <div className="h-full grid grid-cols-3 gap-4">
+                    <div className="h-full grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-4">
                         {PersonalData.competitions.map(CompetitionCard)}
                     </div>
                 </div>
